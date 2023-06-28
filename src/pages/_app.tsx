e2e04ddca4efbd36f7ae4b241eac4f3213cb2994
leetosc <leetosc@hotmail.com>
2023-06-28 01:49:39 -0500

    chore: cleanup and lint fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ThemeProvider } from "~/components/theme-provider";
-import { Header } from "~/components/Navbar2/Navbar2";
-import { SiteHeader } from "~/components/SiteHeader/SiteHeader";
 import Navbar from "~/components/Navbar/Navbar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
